b6829c6d8997e893b20187fd3e56efeb4ce27935
Dustin Forster <dforster@mersive.com>
2021-04-08 11:01:04 -0600

    Deploy PR builds to test server

--- build_windows.ps1
+++ build_windows.ps1
@@ -10,8 +10,18 @@ git fetch --tags
 ErrorOnExeFailure
 
 Write-Host "Configuring with CMake"
-mkdir out
-cd out
+mkdir out32
+cd out32
+cmake .. -G "Visual Studio 15 2017" -DTARGET_CPU=x86 -A Win32 -T host=x64
+ErrorOnExeFailure
+
+Write-Host "Performing Build"
+msbuild libwebrtc.sln /p:Configuration=Release /p:Platform=Win32 /target:ALL_BUILD
+ErrorOnExeFailure
+
+Write-Host "Configuring with CMake"
+mkdir out64
+cd out64
 cmake .. -G "Visual Studio 15 2017" -DTARGET_CPU=x64 -A x64 -T host=x64
 ErrorOnExeFailure
 
